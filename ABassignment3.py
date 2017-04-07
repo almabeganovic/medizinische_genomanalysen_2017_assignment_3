dd258ed8fd26567705c31719696dc2b9297ced66
almabeganovic <alma.beganovic@stud.fh-campuswien.ac.at>
2017-04-07 13:46:45 +0200

    ready to review

--- ABassignment3.py
+++ ABassignment3.py
@@ -43,7 +43,8 @@ class Assignment3:
     
     # the number of identified variants shared by father and son
     def get_variants_shared_by_father_and_son(self):
-
+        self.vcf_reader_father = vcf.Reader(open('AmpliseqExome.20141120.NA24149.vcf', 'r'))
+        self.vcf_reader_son = vcf.Reader(open('AmpliseqExome.20141120.NA24385.vcf', 'r'))
         records = 0
         for record in self.vcf_reader_father:
             if record in self.vcf_reader_son:
@@ -54,7 +55,8 @@ class Assignment3:
 
     # the number of identified variants shared by mother and son
     def get_variants_shared_by_mother_and_son(self):
-
+        self.vcf_reader_mother = vcf.Reader(open('AmpliseqExome.20141120.NA24143.vcf', 'r'))
+        self.vcf_reader_son = vcf.Reader(open('AmpliseqExome.20141120.NA24385.vcf', 'r'))
         records = 0
         for record in self.vcf_reader_mother:
             if record in self.vcf_reader_son:
@@ -65,7 +67,9 @@ class Assignment3:
 
     # the number of identified variants shared by trio
     def get_variants_shared_by_trio(self):
-
+        self.vcf_reader_mother = vcf.Reader(open('AmpliseqExome.20141120.NA24143.vcf', 'r'))
+        self.vcf_reader_father = vcf.Reader(open('AmpliseqExome.20141120.NA24149.vcf', 'r'))
+        self.vcf_reader_son = vcf.Reader(open('AmpliseqExome.20141120.NA24385.vcf', 'r'))
         records = 0
         for record in self.vcf_reader_father:
             if record in self.vcf_reader_son and record in self.vcf_reader_mother:
@@ -74,7 +78,7 @@ class Assignment3:
         print ("-variants_shared_by_trio:")
         print (records)
 
-    # VCF containing all variants of the trio
+    # VCF File with all variants of the trio will be created.Used method walk.together
     def merge_mother_father_son_into_one_vcf(self):
 
         merge_file = open("merge_file.vcf", "w")
@@ -98,13 +102,13 @@ class Assignment3:
 
     def print_summary(self):
         print("All results:")
-        self.get_total_number_of_variants_mother()
-        self.get_total_number_of_variants_father()
-        self.get_variants_shared_by_father_and_son()
-        self.get_variants_shared_by_mother_and_son()
-        self.get_variants_shared_by_trio()
-        self.merge_mother_father_son_into_one_vcf()
-        self.convert_first_variants_of_son_into_HGVS()
+        self.get_total_number_of_variants_mother()     # 38693
+        self.get_total_number_of_variants_father()     # 38641
+        self.get_variants_shared_by_father_and_son()   # 13
+        self.get_variants_shared_by_mother_and_son()   # 1
+        self.get_variants_shared_by_trio()             # 1
+        self.merge_mother_father_son_into_one_vcf()    # merge VCF successfull
+        self.convert_first_variants_of_son_into_HGVS() # not implemented
 
 
 
